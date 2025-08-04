87534886314e4d32748e39590668bad6685886f4
Hári Zalán <harizalan@harizalan.hu>
2025-08-04 06:50:29 +0200

    Now working correctly, little better frontend

--- basic_frontend.py
+++ basic_frontend.py
@@ -1,2 +1,10 @@
 import el_implementation
-print(el_implementation.langton(input()))
+result=el_implementation.langton(input())
+for i in result[0].keys():
+    for j in result[0][i].keys():
+        print(i,j,result[0][i][j],sep=",")
+direction=result[1]
+names={(0,1):"up",(1,0):"right",(0,-1):"down",(-1,0):"left"}
+print()
+print("The ant points ",names[direction])
+print("The ant is currently at (",result[2][0],";",result[2][1],")")
